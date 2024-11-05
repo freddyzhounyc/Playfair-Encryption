f7f59c08a836c3ef39216b078d25efea20d41a91
Freddy Zhou <145275263+freddyzhounyc@users.noreply.github.com>
2024-11-05 18:19:13 -0500

    KeyTable.java

--- KeyTable.java
+++ KeyTable.java
@@ -2,11 +2,6 @@
  * The KeyTable class creates and stores the key which will be used for the encryption and decryption.
  *
  * @author Freddy Zhou
- *      Email: freddy.zhou@stonybrook.edu
- *      Stony Brook ID: 116580337
- *      HW #4 - Playfair Encryption
- *      CSE 214
- *      Recitation Number: R04 | TA Names: Veronica Oreshko, Anuj Sureshbhai, Alex Zuzow
  */
 
 public class KeyTable{
