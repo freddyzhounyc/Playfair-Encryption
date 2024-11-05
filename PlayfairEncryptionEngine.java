bcf4df0180d92fcc2f2776ab282721f1714963b8
Freddy Zhou <145275263+freddyzhounyc@users.noreply.github.com>
2024-11-05 18:19:39 -0500

    PlayfairEncryptionEngine.java

--- PlayfairEncryptionEngine.java
+++ PlayfairEncryptionEngine.java
@@ -2,11 +2,6 @@
  * The PlayfairEncryptionEngine class is the actual runner which combines the operations of Bigram, Phrase, and KeyTable to create an encryption engine.
  *
  * @author Freddy Zhou
- *      Email: freddy.zhou@stonybrook.edu
- *      Stony Brook ID: 116580337
- *      HW #4 - Playfair Encryption
- *      CSE 214
- *      Recitation Number: R04 | TA Names: Veronica Oreshko, Anuj Sureshbhai, Alex Zuzow
  */
 import java.util.Scanner;
 
