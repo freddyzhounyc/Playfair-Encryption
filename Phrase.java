10e52703d5067fcf1524ac6013b5906709af1dea
Freddy Zhou <145275263+freddyzhounyc@users.noreply.github.com>
2024-11-05 18:19:26 -0500

    Phrase.java

--- Phrase.java
+++ Phrase.java
@@ -2,11 +2,6 @@
  * The Phrase class implements a queue using a linked list where it represents the encrypted phrase or decrypted phrase.
  *
  * @author Freddy Zhou
- *      Email: freddy.zhou@stonybrook.edu
- *      Stony Brook ID: 116580337
- *      HW #4 - Playfair Encryption
- *      CSE 214
- *      Recitation Number: R04 | TA Names: Veronica Oreshko, Anuj Sureshbhai, Alex Zuzow
  */
 
 public class Phrase extends BigramQueue{
